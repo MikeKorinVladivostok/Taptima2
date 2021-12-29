fdeb8e437a77b76d8e7b06380000b59793ab68fc
mike <KorinetskiyMikhail@yandex.ru>
2021-12-29 16:39:12 +1000

    сделал FOSUserBundle

---
+++ src/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\UserInterface;
+use http\Client\Response;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function testUserAction( UserInterface $user )
+    {
+        $userManipulator = $this->container->get("fos_user.util.user_manipulator");
+        $userManipulator->addRole($user,'ROLE_ADMIN');
+
+        return new Response();
+    }
+}
